3960d933a3e8636ec1f3ce2550e064d20680dd6b
Chris <chris@wideeyelabs.com>
2013-06-08 16:08:08 -0400

    Add initial chefspec files
    This commit adds the initial ChefSpec files to the cookbook.

---
+++ spec/database_spec.rb
@@ -0,0 +1,8 @@
+require 'chefspec'
+
+describe 'gitlabhq::database' do
+  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'gitlabhq::database' }
+  it 'should do something' do
+    pending 'Your recipe examples go here.'
+  end
+end
---
+++ spec/postgres_spec.rb
@@ -0,0 +1,8 @@
+require 'chefspec'
+
+describe 'gitlabhq::postgres' do
+  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'gitlabhq::postgres' }
+  it 'should do something' do
+    pending 'Your recipe examples go here.'
+  end
+end
