0db59114f5256df8dc9a7684859be89785063aab
Chris <chris@wideeyelabs.com>
2013-06-08 16:36:03 -0400

    Add ssl certs to nginx recipe
    This commit adds ssl certificate and key support to the nginx recipe.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,3 +1,25 @@
+ssl_cert_key = node[:gitlab][:ssl_certificate_key]
+ssl_cert     = node[:gitlab][:ssl_certificate]
+https        = node[:gitlab][:https]
+ssl_req      = node[:gitlab][:ssl_req]
+
+execute "create-ssl-key" do
+  cwd "/etc/nginx"
+  user  'root'
+  group 'root'
+  umask 0077
+  command "openssl genrsa 2048 > #{ssl_cert_key}"
+  not_if { !https || File.exists?(ssl_cert_key) }
+end
+
+execute "create-ssl-cert" do
+  cwd "/etc/nginx"
+  user 'root'
+  group 'root'
+  umask 0077
+  command "openssl req -subj \"#{ssl_req}\" -new -x509 -nodes -sha1 -days 3650 -key #{ssl_cert_key} > #{ssl_cert}"
+  not_if { !https || File.exists?(ssl_cert) }
+end
 
 # Render nginx default vhost config
 template   "/etc/nginx/sites-available/gitlab.conf" do
