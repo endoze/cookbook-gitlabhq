6d4fd4d716826349d1a40398441cecefaba28f4f
Endoze <endoze@endozemedia.com>
2013-06-08 19:02:50 -0400

    Remove unused database spec
    This commit removes an unnecessary spec file from the cookbook.

--- spec/database_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'gitlabhq::database' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'gitlabhq::database' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
-  end
-end
