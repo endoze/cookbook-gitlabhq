6cb607151688b61a06bba091311897d6a931e0e4
Chris <chris@wideeyelabs.com>
2013-08-11 23:28:58 -0400

    Update init script and default recipe
    This commit updates the init script for gitlab service to handle the
    gitlab socket when starting and stopping. It also updates the default
    recipe to remove some unnecessary node attributes that were commented
    out.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,14 +7,6 @@
 # MIT License
 #
 
-# home_dir = node[:gitlab][:home]
-# git_user = node[:gitlab][:user]
-# git_group = node[:gitlab][:group]
-# marker_dir = "#{home_dir}/.markers"
-# ruby_dir = "/usr/local/ruby/#{node[:gitlab][:install_ruby]}/bin"
-# gitlab_home = node[:gitlab][:app_home]
-# backup_path = node[:gitlab][:backup_path]
-
 include_recipe "gitlabhq::dependencies"
 
 include_recipe "gitlabhq::gitlab_users"
