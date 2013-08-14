965646996937c8344e97cb242119daa941e6a7b6
Chris <chris@wideeyelabs.com>
2013-08-13 21:53:48 -0400

    Move dependencies into default attributes
    This commit moves gem and python package dependencies into the default
    attributes file instead of being directly specified in recipes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -77,6 +77,8 @@ default[:gitlab][:https] = true
 default[:gitlab][:ssl_certificate] = "/etc/nginx/#{node[:fqdn]}.crt"
 default[:gitlab][:ssl_certificate_key] = "/etc/nginx/#{node[:fqdn]}.key"
 default[:gitlab][:ssl_req] = "/C=US/ST=Several/L=Locality/O=Example/OU=Operations/CN=#{node[:fqdn]}/emailAddress=root@localhost"
+default[:gitlab][:gems] = %w{ charlock_holmes bundler rake }
+default[:gitlab][:python_packages] = %w{ pygments }
 
 
 
