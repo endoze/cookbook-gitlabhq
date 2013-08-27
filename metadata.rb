3d0d9a04bad77c0023db4321f1593937f375d14b
Chris <chris@wideeyelabs.com>
2013-08-26 22:55:46 -0400

    Update metadata to correct version
    This commit updates the metadata file to correctly show version 0.4.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chris@wideeyelabs.com'
 license          'MIT License'
 description      'Installs/Configures Gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.4.0'
 
 depends 'yumrepo'
 depends 'build-essential'
