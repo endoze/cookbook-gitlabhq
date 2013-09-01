bcc9c1506ee405717e76dc2953f168c7c3375305
Chris Stephan <cstephan@squaremouth.com>
2013-09-01 17:19:22 -0400

    Include data_bag_path in spec helper
    This commit configures chef to use the included data_bags directory as
    part of the chefspec run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,8 @@ POSTGRES_OPTIONS = {
   }
 }
 
+Chef::Config[:data_bag_path] = 'data_bags'
+
 RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
